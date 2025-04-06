1e4b51cc813c342c4619919aed44e4763d13350b
aman kanu <amankanu845@gmail.com>
2025-04-06 21:54:03 +0530

    Initial commit for backend

---
+++ routes/imageRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+import {generateImage} from '../controllers/imgController.js'
+import userAuth from '../middlewares/auth.js'
+
+const imageRouter = express.Router()
+
+imageRouter.post('/generate-image',userAuth,generateImage)
+
+export default imageRouter
